919cf17e99ce7495f3bcd6f34deded26337994d7
leonardo lombardi <lombardi.leo@gmail.com>
2023-01-11 19:20:30 -0300

    Initial implementation

---
+++ MongoDBTesting/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Driver;
+
+Console.WriteLine("Hello, World!");
+
+// This is from Atlas Connect example
+var pass = "sCVOKAhHd1o4E00z";
+var user = "ltlombardi";
+var connString = $"mongodb+srv://{user}:{pass}@cluster0.vj4ysgh.mongodb.net/?retryWrites=true&w=majority";
+var settings = MongoClientSettings.FromConnectionString(connString);
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+var client = new MongoClient(settings);
+
+var dbList = client.ListDatabases().ToList();
+
+Console.WriteLine("The list of databases on this server is: ");
+foreach (var db in dbList)
+{
+    Console.WriteLine(db);
+}
+
+// This is from MongoDb University course Lesson 2
+var mongoURL = new MongoUrl(connString);
+client = new MongoClient(mongoURL);
+
+Console.WriteLine("The list of databases on this server is: ");
+foreach (var db in dbList)
+{
+    Console.WriteLine(db);
+}
