15b5181c71e4c0d8eee3b5f6f1deef635316a739
leonardo lombardi <lombardi.leo@gmail.com>
2023-03-21 11:06:48 -0300

    More operations

--- MongoDBTesting/BasicOperations.cs
+++ MongoDBTesting/BasicOperations.cs
@@ -38,12 +38,25 @@ internal class BasicOperations
         collection.InsertOne(document);
     }
 
+    internal static void InsertTyped(MongoClient client, string databaseName, string collectionName)
+    {
+        var collection = client.GetDatabase(databaseName).GetCollection<BsonDocument>(collectionName);
+        var document = new BsonDocument
+        {
+           { "account_id", "MDB829001337" },
+           { "account_holder", "Linus Torvalds" },
+           { "account_type", "checking" },
+           { "balance", 50352434 }
+        };
+        collection.InsertOne(document);
+    }
+
     internal static void ListItems(MongoClient client, string databaseName, string collectionName)
     {
         var collection = client.GetDatabase(databaseName).GetCollection<BsonDocument>(collectionName);
 
         Console.WriteLine($"The items in the collection {collectionName} are: ");
-        foreach (var item in collection.Find(i => true).ToEnumerable())
+        foreach (var item in collection.Find(_ => true).ToEnumerable())
         {
             Console.WriteLine(item);
         }
---
+++ MongoDBTesting/KeyValuePair.cs
@@ -0,0 +1,10 @@
+﻿
+using MongoDB.Bson;
+
+internal class KeyValuePair
+{
+    public string Key { get; set; }
+    public BsonDocument Value { get; set; }
+    public string Description { get; set; }
+}
+
